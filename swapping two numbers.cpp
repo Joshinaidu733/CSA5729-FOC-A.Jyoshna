f0a85c7c731c105803c2e033645f76cdb1f337bd
Joshinaidu733 <113463935+Joshinaidu733@users.noreply.github.com>
2022-09-20 23:37:15 +0530

    Add files via upload

---
+++ swapping two numbers.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,temp;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	printf("\n before swapping a=%d,b=%d",a,b);
+	temp=a;
+	a=b;
+	b=temp;
+	printf("\n after swapping a=%d,b=%d",a,b);
+	return 0;
+}
