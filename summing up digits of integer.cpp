3388588e00cfb8c48395ece8a9df2d066e2907c6
Joshinaidu733 <113463935+Joshinaidu733@users.noreply.github.com>
2022-09-20 23:39:48 +0530

    Add files via upload

---
+++ summing up digits of integer.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a:");
+	scanf("%d",&a);
+	printf("enter b:");
+	scanf("%d",&b);
+	if(a>b)
+	printf("%d is greater",a);
+	else
+	printf("%d is greater",b);
+}
